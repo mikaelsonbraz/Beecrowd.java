07f03033adb02bec9417c286654760cb87a270c5
mikaelsonbraz <mikaelsonmrn@gmail.com>
2021-12-27 18:45:40 -0300

    beginner 1003

---
+++ src/Beginner/beginner1003.java
@@ -0,0 +1,16 @@
+// https://www.beecrowd.com.br/judge/pt/problems/view/1003
+package Beginner;
+import java.util.Scanner;
+
+public class beginner1003 {
+
+    public static void main(String[] args){
+
+        Scanner entrada = new Scanner(System.in);
+
+        int numA = entrada.nextInt();
+        int numB = entrada.nextInt();
+
+        System.out.println("SOMA = " + (numA + numB));
+    }
+}
