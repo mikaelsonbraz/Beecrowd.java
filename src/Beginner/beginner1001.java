d625a4c0d47f6385bdc260026456bb1c02b4ccb7
mikaelsonbraz <mikaelsonmrn@gmail.com>
2021-12-27 13:48:51 -0300

    first commit && first challenge

---
+++ src/Beginner/beginner1001.java
@@ -0,0 +1,16 @@
+package Beginner;
+import java.util.Scanner;
+
+public class beginner1001 {
+
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        int numA = entrada.nextInt();
+        int numB = entrada.nextInt();
+
+        entrada.close();
+
+        System.out.println("X = " + (numA+numB));
+    }
+}
