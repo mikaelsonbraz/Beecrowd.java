39013e860dd355e9f81c7fe675f68b85257672fe
mikaelsonbraz <mikaelsonmrn@gmail.com>
2021-12-30 11:08:21 -0300

    beginner 1004

---
+++ src/Beginner/beginner1004.java
@@ -0,0 +1,17 @@
+// https://www.beecrowd.com.br/judge/en/problems/view/1004
+package Beginner;
+
+import java.util.Scanner;
+
+public class beginner1004 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int valor1 = entrada.nextInt();
+        int valor2 = entrada.nextInt();
+
+        System.out.println("PROD = " + (valor1 * valor2));
+    }
+}
