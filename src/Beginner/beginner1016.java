c10017ba53dc1be5aa183406f82a37b97587e4fd
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-01-04 10:40:05 -0300

    Beginner 1016

---
+++ src/Beginner/beginner1016.java
@@ -0,0 +1,15 @@
+// https://www.beecrowd.com.br/judge/en/problems/view/1016
+package Beginner;
+import java.util.Scanner;
+
+public class beginner1016 {
+
+    public static void main(String[] args){
+
+        Scanner ent = new Scanner(System.in);
+
+        int distancia = ent.nextInt();
+
+        System.out.println(distancia * 2 + " minutos");
+    }
+}
