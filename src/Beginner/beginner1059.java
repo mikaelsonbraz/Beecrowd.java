0b02ffeea6a9007e78d8c0578ad081302fa44c39
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-01-13 18:07:57 -0300

    Beginner 1059

---
+++ src/Beginner/beginner1059.java
@@ -0,0 +1,12 @@
+// https://www.beecrowd.com.br/judge/en/problems/view/1059
+package Beginner;
+
+public class beginner1059 {
+
+    public static void main(String[] args){
+
+        for (int i = 2; i <= 100; i+=2){
+            System.out.println(i);
+        }
+    }
+}
