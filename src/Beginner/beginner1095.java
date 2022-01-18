d36ea0e7327032ae61c92bc0caf37290f13d610b
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-01-18 13:59:25 -0300

    Beginner 1095

---
+++ src/Beginner/beginner1095.java
@@ -0,0 +1,14 @@
+package Beginner;
+
+public class beginner1095 {
+
+    public static void main(String[] args){
+
+        int j = 60;
+
+        for(int i = 1; j >= 0; i+=3){
+            System.out.println("I=" + i + " J=" +j);
+            j -= 5;
+        }
+    }
+}
