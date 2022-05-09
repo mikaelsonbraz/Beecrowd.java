20a1f370f2a6f9062d918e53245fd304ba4d0dbd
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-05-09 11:51:26 -0300

    beginner 1187

---
+++ src/Beginner/beginner1187.java
@@ -0,0 +1,36 @@
+package Beginner;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class beginner1187 {
+
+public static void main(String[] args) throws IOException {
+
+    Scanner leitor = new Scanner(System.in);
+
+    double soma = 0;
+    char O = leitor.next().toUpperCase().charAt(0);
+
+    double[][] M = new double[12][12];
+
+    for (int i = 0; i < M.length; i++) {
+        for (int j = 0; j < M[i].length; j++) {
+
+            M[i][j] = leitor.nextDouble();
+        }
+    }
+
+    for (int i = 0; i < M.length; i++) {
+        for (int j = 0; j < M[i].length; j++) {
+
+            if (j > i && j < M.length-i-1) soma += M[i][j];
+
+        }
+    }
+
+    if (O == 'M') soma /= 30;
+    System.out.printf("%.1f%n", soma);
+}
+
+}
