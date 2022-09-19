5ddf8de12ffa00175e5e6805e81590206ee0564a
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-09-19 15:29:14 -0300

    Finally solved - Beginner 1021

--- src/Beginner/beginner1021.java
+++ src/Beginner/beginner1021.java
@@ -1,58 +1,56 @@
 // https://www.beecrowd.com.br/judge/en/problems/view/1021
 package Beginner;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class beginner1021 {
 
-    public static void main(String[] args){
+    static int nota100 = 0, nota50 = 0, nota20 = 0, nota10 = 0, nota5 = 0, nota2 = 0;
+    static int moeda100 = 0, moeda50 = 0, moeda25 = 0, moeda10 = 0, moeda5 = 0, moeda1 = 0;
+
+    private static int counterValor(float valor, float dinheiro){
+        int counter = 0;
+        while (valor >= dinheiro){
+            valor -= dinheiro;
+            counter += 1;
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) throws IOException {
 
         Scanner ent = new Scanner(System.in);
 
-        double valor = ent.nextDouble();
+        float valor = ent.nextFloat();
+        float sobra;
 
-        int nota100 = 0, nota50 = 0, nota20 = 0, nota10 = 0, nota5= 0, nota2 = 0;
-        int moeda100 = 0, moeda50 = 0, moeda25 = 0, moeda10 = 0, moeda5 = 0, moeda1 = 0;
+        nota100 = counterValor(valor, 100);
+        sobra = valor - (nota100 * 100);
+        nota50 = counterValor(sobra, 50);
+        sobra = sobra - (nota50 * 50);
+        nota20 = counterValor(sobra, 20);
+        sobra = sobra - (nota20 * 20);
+        nota10 = counterValor(sobra, 10);
+        sobra = sobra - (nota10 * 10);
+        nota5 = counterValor(sobra, 5);
+        sobra = sobra - (nota5 * 5);
+        nota2 = counterValor(sobra, 2);
+        sobra = sobra - (nota2 * 2);
 
-        while (valor >= 0.01){
-            if (valor >= 100){
-                valor -= 100;
-                nota100 += 1;
-            } else if (valor >= 50){
-                valor -= 50;
-                nota50 += 1;
-            } else if (valor >= 20){
-                valor -= 20;
-                nota20 += 1;
-            } else if (valor >= 10){
-                valor -= 10;
-                nota10 += 1;
-            } else if (valor >= 5){
-                valor -= 5;
-                nota5 += 1;
-            } else if (valor >= 2){
-                valor -= 2;
-                nota2 += 1;
-            } else if (valor >= 1){
-                valor -= 1;
-                moeda100 += 1;
-            } else if (valor >= 0.5){
-                valor -= 0.5;
-                moeda50 += 1;
-            } else if (valor >= 0.25){
-                valor -= 0.25;
-                moeda25 += 1;
-            } else if (valor >= 0.1){
-                valor -= 0.1;
-                moeda10 += 1;
-            } else if (valor >= 0.05){
-                valor -= 0.05;
-                moeda5 += 1;
-            } else {
-                valor -= 0.01;
-                moeda1 += 1;
-            }
-        }
+        moeda100 = counterValor(sobra, 1);
+        sobra = sobra - (moeda100);
+        moeda50 = counterValor(sobra, 0.5F);
+        sobra = (float) (sobra - (moeda50 * 0.5));
+        moeda25 = counterValor(sobra, 0.25F);
+        sobra = (float) (sobra - (moeda25 * 0.25));
+        moeda10 = counterValor(sobra, 0.1F);
+        sobra = (float) (sobra - (moeda10 * 0.1));
+        moeda5 = counterValor(sobra, 0.05F);
+        sobra = (float) (sobra - (moeda5 * 0.05));
+        moeda1 = counterValor(sobra, 0.01F);
+        sobra = (float) (sobra - (moeda1 * 0.01));
+
 
         System.out.println("NOTAS:\n" +
                 nota100 + " nota(s) de R$ 100.00\n" +
@@ -69,4 +67,6 @@ public class beginner1021 {
                 moeda5 + " moeda(s) de R$ 0.05\n" +
                 moeda1 + " moeda(s) de R$ 0.01");
     }
+
 }
+
